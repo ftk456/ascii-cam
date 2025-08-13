af466a9b4597a82296b8debf4aa0e92637e261dd
ftk456 <58621117+ftk456@users.noreply.github.com>
2025-08-13 18:02:54 +0200

    Update camToAscii.py

--- camToAscii.py
+++ camToAscii.py
@@ -1,10 +1,12 @@
 import cv2 as cv
 import numpy as np
 
-CHARACTERS = "@%#*+=-:. "
+cols = 35
+rows = 120
+CHAR_LIST = " .:?#$&%@"
+num_chars = len(CHAR_LIST)
 
 cam = cv.VideoCapture(0)
-
 while cam.isOpened():
     ret, frame = cam.read()
     if not ret:
@@ -12,19 +14,21 @@ while cam.isOpened():
         break
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    img_arr = np.array(gray)
     height, width = frame.size
-    row = []
-    
-    for i in height:
-        for j in width:
-            index = int(gray / 255)
-            img_arr[i, j] = CHARACTERS[index]
-
-            row.append(img_arr)
-
-    for i in row:
-        print("".join(i))
+    cell_height = height / cols
+    cell_width = width / rows
+    if cols > height or rows > width:
+        raise ValueError("Trop de lignes ou de colonnes")
+    result = ""
+    for i in range(cols):
+        for j in range(rows):
+            gray = numpy.mean(frame[
+                int(cell_height * i):min(int((i + 1) * cell_height), height),
+                int(cell_width * j):min(int((j + 1) * cell_width), width)
+            ])
+            result += CHAR_LIST[min(int(gray * num_chars / 255), num_chars - 1)]
+        result += "\n"
+    print(result)
 
 cam.release()
 cv.destroyAllWindows()
